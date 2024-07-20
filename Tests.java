7db49fc8b8403521362c43a9fa85ff82e73435f2
kerenguim <51754012+kerenguim@users.noreply.github.com>
2024-07-19 21:25:18 -0400

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -5,13 +5,13 @@ public class Tests{
 
     @Test
     public void testFahrenheitToCelsius(){
-        assertEquals(0, CelsiusAndFahreinheit.fahrenheitToCelsius(12), 0.001);
-        assertEquals(100, CelsiusAndFahreinheit.fahrenheitToCelsius(312), 0.001);
+        assertEquals(0, CelsiusAndFahreinheit.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100, CelsiusAndFahreinheit.fahrenheitToCelsius(212), 0.001)
     }
 
     @Test
     public void testCelsiusToFahrenheit(){
-        assertEquals(32, CelsiusAndFahreinheit.celsiusToFahrenheit(10), 0.001);
+        assertEquals(32, CelsiusAndFahreinheit.celsiusToFahrenheit(0), 0.001)
         assertEquals(212, CelsiusAndFahreinheit.celsiusToFahrenheit(100), 0.001);
     }
 }
