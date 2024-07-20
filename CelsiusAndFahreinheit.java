b36964199d33d1ffb506e19377ed6332daeb829b
kerenguim <51754012+kerenguim@users.noreply.github.com>
2024-07-19 21:16:08 -0400

    Update CelsiusAndFahreinheit.java

--- CelsiusAndFahreinheit.java
+++ CelsiusAndFahreinheit.java
@@ -1,7 +1,7 @@
-public clas CelsiusAndFahreinheit{
+public class CelsiusAndFahreinheit{
 
     public static double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / ;
+        return (fahrenheit - 32) * 5 / 9;
     }
 
     public static double celsiusToFahrenheit(double celsius) {
