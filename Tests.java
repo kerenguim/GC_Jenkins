59e273ad7f6841a8a54a67882bed70fc50b5cf10
kerenguim <51754012+kerenguim@users.noreply.github.com>
2024-07-19 21:15:48 -0400

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 public class Tests{
 
     @Test
-    publico void testFahrenheitToCelsius(){
-        cassertEquals(0, CelsiusAndFahreinheit.fahrenheitToCelsius(32), 0.001);
-        cassertEquals(100, CelsiusAndFahreinheit.fahrenheitToCelsius(212), 0.001);
+    public void testFahrenheitToCelsius(){
+        assertEquals(0, CelsiusAndFahreinheit.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100, CelsiusAndFahreinheit.fahrenheitToCelsius(212), 0.001);
     }
 
     @Test
-    publico void testCelsiusToFahrenheit(){
-        cassertEquals(32, CelsiusAndFahreinheit.celsiusToFahrenheit(0), 0.001);
-        cassertEquals(212, CelsiusAndFahreinheit.celsiusToFahrenheit(100), 0.001);
+    public void testCelsiusToFahrenheit(){
+        assertEquals(32, CelsiusAndFahreinheit.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212, CelsiusAndFahreinheit.celsiusToFahrenheit(100), 0.001);
     }
 }
