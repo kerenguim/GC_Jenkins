7360883739765673ec59c70a98e3b13debfb9c57
kerenguim <51754012+kerenguim@users.noreply.github.com>
2024-07-19 21:04:24 -0400

    cenario 2

--- CelsiusAndFahreinheit.java
+++ CelsiusAndFahreinheit.java
@@ -1,7 +1,7 @@
 public class CelsiusAndFahreinheit{
 
     public static double fahrenheitToCelsius(double fahrenheit) {
-        return (fahrenheit - 32) * 5 / 9;
+        return (fahrenheit - 32) * 5 / ;
     }
 
     public static double celsiusToFahrenheit(double celsius) {
